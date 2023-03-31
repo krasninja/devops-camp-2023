b2fa00128a4e98d69c296b93d83ad68c82b77e24
Ivan Kozhin <ivan@saritasa.com>
2023-03-31 10:13:48 +0700

    fix: use cat, use constants

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
-if [ "$#" -eq 0 ]; then
+#
+# Processes repositories list, generates the ed25519 key for each of them and
+# outputs the Kustomization configuration.
+set -eo pipefail
+
+if [ $# -eq 0 ]; then
   echo "Usage: chapter2-t2-gen-kustomization.sh [REPO]..."
   exit 1
 fi
 
 readonly DEPLOY_KEY_POSTFIX="deploy-key.pem"
+readonly REPO_DIR="repos"
+readonly KUSTOMIZATION_FILE="./$REPO_DIR/kustomization.yaml"
 
-# stop on first error
-set -e
-
-# init, prepare kustomize
+# Init, prepare kustomize.
 mkdir -p repos
-echo \
-"apiVersion: kustomize.config.k8s.io/v1beta1
+cat <<EOF > "$KUSTOMIZATION_FILE"
+apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 
 generatorOptions:
   disableNameSuffixHash: true
 
-secretGenerator:"  > ./repos/kustomization.yaml
+secretGenerator:
+EOF
 
-# iter thru repos
 while (( $# )); do
-  repo=$(basename $1)
-  deploy_key="./repos/$repo-$DEPLOY_KEY_POSTFIX"
-  
-  # create deploy key
-  rm -f $deploy_key
-  ssh-keygen -t ed25519 -f $deploy_key -N '' -m pem -q
+  repo=$(basename "$1")
+  deploy_key="./$REPO_DIR/$repo-$DEPLOY_KEY_POSTFIX"
 
-  # append kustomization data
-  echo -e \
-"  - name: $repo
+  # Create deploy key.
+  ssh-keygen -t ed25519 -f "$deploy_key" -N '' -m pem -q <<< y > /dev/null 2>&1
+
+  # Append kustomization data.
+  cat <<EOF >> "$KUSTOMIZATION_FILE"
+  - name: $repo
     namespace: argo-cd
     options:
       labels:
@@ -42,10 +45,12 @@ while (( $# )); do
       - type=git
       - project=default
     files:
-      - sshPrivateKey=$repo-$DEPLOY_KEY_POSTFIX\n" >> ./repos/kustomization.yaml
+      - sshPrivateKey=$repo-$DEPLOY_KEY_POSTFIX
+
+EOF
 
   shift
 done
 
+# Generate final output.
 kustomize build repos
-
