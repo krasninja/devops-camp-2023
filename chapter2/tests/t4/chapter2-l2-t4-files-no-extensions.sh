ec6906a6abeb029fee96ad5fee9f326a47f08d90
Ivan Kozhin <ivan@saritasa.com>
2023-04-01 17:23:24 +0700

    fix: improve files processing

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #
 # The scripts returns list of files without extension.
+set -eou pipefail
 
-file=${1?"Usage: [DIR]"}
-for file in $(find "${file}" -type f 2>/dev/null); do echo "${file%.*}"; done
+IFS=$'\n'
+
+dir=${1?"Usage: [DIR]"}
+for file in $(find "${dir}" -type f 2>/dev/null); do
+  echo "${file%.*}";
+done
 
