2864fabbdf7f39f90be92527c5973d164eee44c9
Ivan Kozhin <ivan@saritasa.com>
2023-04-03 12:32:45 +0700

    fix: Validate repository name

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 
 readonly DEPLOY_KEY_POSTFIX="deploy-key.pem"
 readonly REPO_DIR="repos"
-readonly KUSTOMIZATION_FILE="./$REPO_DIR/kustomization.yaml"
+readonly KUSTOMIZATION_FILE="./${REPO_DIR}/kustomization.yaml"
 
 if [ $# -eq 0 ]; then
   echo "Usage: $(basename "$0") [REPO]..."
@@ -15,7 +15,7 @@ fi
 
 # Init, prepare kustomize.
 mkdir -p "${REPO_DIR}"
-cat <<EOF > "$KUSTOMIZATION_FILE"
+cat <<EOF > "${KUSTOMIZATION_FILE}"
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 
@@ -27,13 +27,20 @@ EOF
 
 while (( $# )); do
   repo=$(basename "${1}")
-  deploy_key="./$REPO_DIR/$repo-$DEPLOY_KEY_POSTFIX"
+
+  # Validate repository name.
+  if [[ ! "${repo}" =~ ^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$ ]]; then
+    echo "Invalid repository name '${repo}'" >&2
+    exit 1
+  fi
+
+  deploy_key="./${REPO_DIR}/${repo}-${DEPLOY_KEY_POSTFIX}"
 
   # Create deploy key.
   ssh-keygen -t ed25519 -f "${deploy_key}" -N '' -m pem -q <<< y > /dev/null 2>&1
 
   # Append kustomization data.
-  cat <<EOF >> "$KUSTOMIZATION_FILE"
+  cat <<EOF >> "${KUSTOMIZATION_FILE}"
   - name: $repo
     namespace: argo-cd
     options:
