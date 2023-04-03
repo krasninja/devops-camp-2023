fb387159cb09bdea2077e108ea65bd6f85d8f67e
Ivan Kozhin <ivan@saritasa.com>
2023-04-03 11:16:06 +0700

    feat: Add content file name

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -4,6 +4,9 @@
 # create it with permission 700 with bash64 password.
 set -eou pipefail
 
+YELLOW_COLOR='\033[0;33m' 
+NO_COLOR='\033[0m'
+
 if [ $# -lt 2 ]; then
   echo "The script requires at least 2 arguments - filenames"
   exit 1
@@ -11,9 +14,10 @@ fi
 
 # Iterate thru files.
 while (( $# )); do
-  file="$1"
+  file="${1}"
   # If exists - show content.
   if [ -e "${file}" ]; then
+    echo -e "${YELLOW_COLOR}Content of file '${file}'${NO_COLOR}"
     cat "${file}"
     echo ''
   # If not - create with random base64 string and permissions rwx------.
