e327ef503a149049dbafb494aae40a9656f07c6b
Ivan Kozhin <ivan@saritasa.com>
2023-04-04 18:58:47 +0700

    feat: Check that configuration exists

--- chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
+++ chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
@@ -6,8 +6,25 @@ set -eou pipefail
 readonly VELERO_CONFIG='https://gist.githubusercontent.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/030a82af2489bc353cec370779cfd06852668e29/velero.yaml'
 readonly KUBE_NAMESPACES='https://gist.githubusercontent.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/0e57e62984b69ff16c9b9159776e145f6d4feecf/kubernetes-namespaces.txt'
 
+#######################################
+# Request URI and checks that content is available.
+# The function terminates the script in case of bad status code.
+# Arguments:
+#   URI.
+#######################################
+function ensure_uri_exists {
+  local status_code=$(curl -I --silent "${1}" | head -n 1 | cut -d' ' -f2)
+  if [[ "${status_code}" != 200 ]]; then
+    echo "Cannot download URI '${1}', status code ${status_code}." >&2
+    exit 1
+  fi
+}
+
+ensure_uri_exists "${VELERO_CONFIG}"
+ensure_uri_exists "${KUBE_NAMESPACES}"
+
 comm -13 \
   <(curl --silent "${VELERO_CONFIG}" | yq '.spec.source.helm.values' -r | yq '.schedules[].template.includedNamespaces[]' -r | sort | uniq) \
   <(curl --silent "${KUBE_NAMESPACES}" | sort | uniq) \
-  | sed 's/\([[:alnum:]]*\)/- \1/'
+  | sed 's/^/- /'
 
