69c077dbe4df2c6ccce7fdc0a669b1403ab683c1
Ivan Kozhin <ivan@saritasa.com>
2023-04-28 21:47:18 +0700

    fix: New line fix

--- chapter4/tests/t15/tcpdump.sh
+++ chapter4/tests/t15/tcpdump.sh
@@ -7,4 +7,3 @@ readonly DEBUG_CONTAINER_NAME="nginx-debug"
 
 docker start "${CONTAINER_NAME}" || docker run --rm --publish 8080:80 -d --name "${CONTAINER_NAME}" nginx
 docker run --rm -it --name "${DEBUG_CONTAINER_NAME}" --network container:"${CONTAINER_NAME}" nixery.dev/tcpdump tcpdump -i eth0 -A -nn port 80
-
