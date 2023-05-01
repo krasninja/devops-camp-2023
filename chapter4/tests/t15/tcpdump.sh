1e52c3f5b0c9c89d0cf5a52edcfcb2fd4db8e923
Ivan Kozhin <ivan@saritasa.com>
2023-05-01 17:31:05 +0700

    fix: Use only necessary caps

--- chapter4/tests/t15/tcpdump.sh
+++ chapter4/tests/t15/tcpdump.sh
@@ -6,4 +6,4 @@ readonly CONTAINER_NAME="nginx"
 readonly DEBUG_CONTAINER_NAME="nginx-debug"
 
 docker start "${CONTAINER_NAME}" || docker run --rm --publish 8080:80 -d --name "${CONTAINER_NAME}" nginx
-docker run --rm -it --name "${DEBUG_CONTAINER_NAME}" --network container:"${CONTAINER_NAME}" nixery.dev/tcpdump tcpdump -i eth0 -A -nn port 80
+docker run --rm -it --cap-add="NET_ADMIN" --cap-add="NET_RAW" --cap-drop="ALL" --name "${DEBUG_CONTAINER_NAME}" --network container:"${CONTAINER_NAME}" nixery.dev/tcpdump tcpdump -i eth0 -A -nn port 80
