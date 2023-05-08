68f10efa37f91ca9e7ea1ef79635a27b2b95ab02
Ivan Kozhin <ivan@saritasa.com>
2023-05-08 14:15:10 +0700

    fix: Apply sort for comm

--- chapter2/tests/t9/chapter2-l4-t9-get-tcp-listening-connections-in-terminator.sh
+++ chapter2/tests/t9/chapter2-l4-t9-get-tcp-listening-connections-in-terminator.sh
@@ -9,25 +9,27 @@ readonly PORT_START=10000
 readonly PORT_END=10100
 readonly FILTER_EXPR="( sport >= ${PORT_START} && sport <= ${PORT_END} )"
 
-total_ss_output=$(ss -ltn "${FILTER_EXPR}" --no-header)
+total_ss_output=$(ss -ltnH "${FILTER_EXPR}" | sort)
 echo "Looking for the new listening TCP sockets on ports ${PORT_START}-${PORT_END} range..."
 
 while true; do
   # Compare last tracked connections and current ones. We want to display only new connections.
-  ss_output=$(ss -ltn "${FILTER_EXPR}" --no-header)
-  ss_diff=$( comm -13 <(echo "${total_ss_output}") <(echo "${ss_output}") )
+  ss_output=$(ss -ltnH "${FILTER_EXPR}" | sort)
+  ss_diff=$(comm -13 <(echo "${total_ss_output}") <(echo "${ss_output}"))
 
   # Display diff to active ttys.
   if [[ -n "${ss_diff}" ]]; then
-    terminator_pid=$(pgrep terminator)
-    readonly terminator_pid
+    terminator_pid=$(pgrep terminator) || true
     if [[ -n "${terminator_pid}" ]]; then
-      terminator_zsh_pid=$(pgrep -P "${terminator_pid}" zsh)
-      readonly terminator_zsh_pid
-      $(echo "${ss_diff}" | awk '{ printf "%-20s %-20s\n", $4, $5 }' 2>/dev/null > "/proc/${terminator_zsh_pid}/fd/1") || true
+      terminator_zsh_pid=$(pgrep -P "${terminator_pid}" zsh) || true
+      if [[ -n "${terminator_zsh_pid}" ]]; then
+        echo "${ss_diff}" | awk '{ printf "%-20s %-20s\n", $4, $5 }' 2>/dev/null > "/proc/${terminator_zsh_pid}/fd/1" || true
+      else
+        echo 'Cannot find zsh PID!' >&2
+      fi
     fi
   fi
 
-  total_ss_output="${ss_output}"
+  total_ss_output=$(echo "${ss_output}" | sort)
   sleep "${SLEEP_TIMEOUT_SEC}"
 done
